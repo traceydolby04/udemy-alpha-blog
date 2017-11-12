df6269be1b001f2fb112123713c836977e41fb77
Rails Udemy course <tdrailsudemycourse@gmail.com>
2017-11-11 17:52:48 -0800

    Fix create add flash and error messages, create show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,15 +8,24 @@ class ArticlesController < ApplicationController
 # This allows us to go to articles/create URL
   def create
     # to display what's being passed in from articles/new form
-       #  render plain: params[:article].inspect
-
-    # to save what is being passed in from articles/new form
+    # better way to write to save what is being passed in from articles/new form
     # we need to create a new instance variable and whitelist
-    #the values of article
-
+    # the values of article
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+
+    if @article.save
+      flash[:notice] = "Article was successfully created"
+      redirect_to article_path(@article)
+      # display or prevent blank articles from being saved this is done in
+      # articles/new.html.erb file
+    else
+      render 'new'
+    end
+
+  end
+  #grabbing from the params hash to find id
+  def show
+    @article = Ariticle.find(params[:id])
   end
 
   # This allows us to save the keys from the information we receive
