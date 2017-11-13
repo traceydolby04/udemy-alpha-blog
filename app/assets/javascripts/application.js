445e575b6db1563bb7555e8f4e8eb5c4585b244f
Rails Udemy course <tdrailsudemycourse@gmail.com>
2017-11-12 21:06:44 -0800

    adding Css styling to home page and adding nav bar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,15 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
+
 //= require jquery
-//= require jquery-ujs
+// Had to uncomment the jquery line below for application to work
+// because of a recent update to rails 5.1 and not needing it
+// require jquery-ujs
 //= require rails-ujs
 //= require bootstrap-sprockets
 //= require turbolinks
-//= require_tree
+//= require_tree .
+
 
